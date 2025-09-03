3ffc0e6fc9c97d8a8d265fdaf5400f8ec288296f
SaiCarthikeya <saicarthik180@gmail.com>
2025-09-03 21:10:53 +0530

    fetch and display words

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,8 @@
 const Header = () => {
     return (
-        <div className="h-20 bg-blue-500 flex justify-between p-2">
+        <div className="h-20 bg-blue-500 flex justify-between p-6 m-0">
             <p className="text-white text-shadow-blue-500 text-2xl">TYPESPEED</p>
+            <p>lol</p>
         </div>
     )
 }
--- client/src/pages/TypingView.tsx
+++ client/src/pages/TypingView.tsx
@@ -1,25 +1,75 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect }from 'react'
+
+type STATUS = 'CORRECT' | 'MISTAKE' | 'UNTYPED';
+
+type WordStatus = {
+    word: string;
+    status: STATUS;
+}
 
 const TypingView = () => {
-    const [typedText, setTypedText] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(true);
-    const [targetText, setTargetText] = useState<String>("");
-    const [words, setWords] = useState<Array<string>>([]);
+    const [userInput, setUserInput] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [wordList, setWordList] = useState<Array<string>>([]);
+    const [wordStatusList, setWordStatusList] = useState<Array<WordStatus>>([]);
 
+    const fetchRandomWords = async (wordCount: Number) => {
+        try {
+            setIsLoading(true);
+            const apiUrl: string = `https://random-word-api.herokuapp.com/word?number=${wordCount}`
+            const apiResponse = await fetch(apiUrl);
+            const fetchedWords = await apiResponse.json();
+            setWordList(fetchedWords);
+            const initializedWordStatusList: Array<WordStatus> = fetchedWords.map((word: string) => ({ word: word, status: "UNTYPED" }));
+            setWordStatusList(initializedWordStatusList)
+        } catch (e) {
+            console.error("Something went wrong!! :<", e)
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     useEffect(() => {
-        const result = fetch("https://random-word-api.herokuapp.com/word?number=50").then((res) => res.json()).then((json_res) => {
-            console.log(json_res);
-        });
-        // const json_result = await result.json();
-        
-        console.log(result) 
+        fetchRandomWords(50);
     }, [])
+
+    const keyPressed = (e : React.ChangeEvent<HTMLInputElement >) => {
+        if (e.target && e.target.value == ' ') {
+            console.log("space clicked")
+        }
+        
+    }
+
+    // useEffect(() => {
+    //     const setText = () => {
+    //         let text : string = "";
+    //         for (let word of words) {
+    //             text += word + " ";
+    //         }
+    //         setTargetText(text);
+    //     }
+    //     setText();
+    // }, [words])
 
     return (
         <div className='flex flex-col justify-center items-center rounded-3xl shadow-blue-300 bg-blue-900'>
-            {loading ? <p>Loading...</p> : <p>{targetText}</p>}
-            <input value={typedText} onChange={(e) => setTypedText(e.target.value)}/>
+            {isLoading ? <p>Loading...</p> :
+            (
+                <p className='p-3 text-2xl'>
+                    {wordStatusList.map((wordStatus : WordStatus) => {
+                        switch(wordStatus.status) {
+                            case 'CORRECT':
+                                return <span className='text-green-400'>{wordStatus.word} </span>
+                            case 'MISTAKE':
+                                return <span className='text-red-400'>{wordStatus.word} </span>
+                            case 'UNTYPED':
+                                return <span className='text-gray-400'>{wordStatus.word} </span>
+                        }
+                    })}
+                </p>
+            ) 
+            }
+            <input value={userInput} onChange={(e) => keyPressed(e)} className='bg-blue-300 border-none rounded'/>
         </div>
     )
 }
