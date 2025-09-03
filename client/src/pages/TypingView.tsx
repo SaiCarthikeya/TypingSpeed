c72e75819343ae8b00ffbd240f35534b658c099a
SaiCarthikeya <saicarthik180@gmail.com>
2025-09-03 22:20:36 +0530

    Correct Wrong Current Word Coloring

--- client/src/pages/TypingView.tsx
+++ client/src/pages/TypingView.tsx
@@ -11,6 +11,7 @@ const TypingView = () => {
     const [userInput, setUserInput] = useState<string>("");
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [wordList, setWordList] = useState<Array<string>>([]);
+    const [wordIndex, setWordIndex] = useState<number>(0);
     const [wordStatusList, setWordStatusList] = useState<Array<WordStatus>>([]);
 
     const fetchRandomWords = async (wordCount: Number) => {
@@ -33,10 +34,23 @@ const TypingView = () => {
         fetchRandomWords(50);
     }, [])
 
-    const keyPressed = (e : React.ChangeEvent<HTMLInputElement >) => {
-        if (e.target && e.target.value == ' ') {
-            console.log("space clicked")
+    const wordTyped = () => {
+
+        console.log("space clicked")
+        const wordStatusListCopy = wordStatusList;
+        const userWord = userInput.trim();
+        if (userWord === '') {
+            return;
         }
+        console.log(userWord + "  " +  wordList[wordIndex])
+        if (userWord === wordList[wordIndex]) {
+            wordStatusListCopy[wordIndex].status = 'CORRECT';
+        } else {
+            wordStatusListCopy[wordIndex].status = 'MISTAKE';
+        }
+        setWordStatusList(wordStatusListCopy);
+        setUserInput('');
+        setWordIndex((prevIdx) => prevIdx + 1);
         
     }
 
@@ -56,20 +70,29 @@ const TypingView = () => {
             {isLoading ? <p>Loading...</p> :
             (
                 <p className='p-3 text-2xl'>
-                    {wordStatusList.map((wordStatus : WordStatus) => {
+                    {wordStatusList.map((wordStatus : WordStatus, idx : number) => {
+                        if (idx == wordIndex) {
+                            return <span className='text-blue-400' key={idx}>{wordStatus.word} </span>
+                        }
                         switch(wordStatus.status) {
                             case 'CORRECT':
-                                return <span className='text-green-400'>{wordStatus.word} </span>
+                                return <span className='text-green-400' key={idx}>{wordStatus.word} </span>
                             case 'MISTAKE':
-                                return <span className='text-red-400'>{wordStatus.word} </span>
+                                return <span className='text-red-400' key={idx}>{wordStatus.word} </span>
                             case 'UNTYPED':
-                                return <span className='text-gray-400'>{wordStatus.word} </span>
+                                return <span className='text-gray-400' key={idx}>{wordStatus.word} </span>
                         }
                     })}
                 </p>
             ) 
             }
-            <input value={userInput} onChange={(e) => keyPressed(e)} className='bg-blue-300 border-none rounded'/>
+            <input value={userInput} onChange={(e) => setUserInput(e.target.value)} onKeyDown={(e) => {
+                const keyCode = e.keyCode;
+                console.log(keyCode)
+                if (keyCode === 13 || keyCode === 32) {
+                    wordTyped();
+                }
+            }} className='bg-blue-300 border-none rounded'/>
         </div>
     )
 }
